ddc647c14339f9a61151c3fcb6ff79f4e30aeede
maritzaalamo <39917899+maritzaalamo@users.noreply.github.com>
2020-02-24 08:27:39 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,29 @@
 function textAppear(){
-  const text = document.querySelector('#para1');
+  var text = document.querySelector('#para1');
 
-  let textPosition = text.getBoundingClientRect().top;
+  var textPosition = text.getBoundingClientRect().top;
 
-  let screenHeight = window.innerHeight;
+  var screenHeight = window.innerHeight;
 
   if(textPosition < (screenHeight-(screenHeight/4))){
     text.classList.add('appear');
   }
 }
 function textAppear2(){
-  const text = document.querySelector('#para2');
+  var text = document.querySelector('#para2');
 
-  let textPosition = text.getBoundingClientRect().top;
+  var textPosition = text.getBoundingClientRect().top;
 
-  let screenHeight = window.innerHeight;
+  var screenHeight = window.innerHeight;
 
   if(textPosition < (screenHeight-(screenHeight/4))){
     text.classList.add('appear2');
   }
 }
 
-let nav = document.querySelector('nav');
-let banner = document.querySelector('.banner');
-let bannerHeight = banner.getBoundingClientRect().bottom;
+function navbarAppear(){
 
-
-window.addEventListener('scroll', function(event){
-  event.preventDefault();
-  if(window.scrollY >= bannerHeight) {
-    nav.style.backgroundColor = '#3155A4';
-  } else {
-    nav.style.backgroundColor = 'transparent';
-  }
-});
+}
 
 window.addEventListener('scroll', textAppear);
 window.addEventListener('scroll', textAppear2);
